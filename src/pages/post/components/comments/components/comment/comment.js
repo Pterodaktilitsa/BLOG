ef9509019ef9a9b549e041b2f8421139a700c00c
Pterodaktilitsa <pterodaktilitsa@gmail.com>
2023-11-09 20:11:52 +0300

    Реализовать модальное окно и удаление комментария

--- src/pages/post/components/comments/components/comment/comment.js
+++ src/pages/post/components/comments/components/comment/comment.js
@@ -1,7 +1,25 @@
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { openModal, CLOSE_MODAL, removeCommentAsync } from '../../../../../../actions';
 import { Icon } from '../../../../../../components';
+import { useServerRequest } from '../../../../../../hooks';
 
-const CommentContainer = ({ className, id, author, publishedAt, content }) => {
+const CommentContainer = ({ className, postId, id, author, publishedAt, content }) => {
+	const dispatch = useDispatch();
+	const requestServer = useServerRequest();
+
+	const onCommentRemove = (id) => {
+		dispatch(
+			openModal({
+				text: 'Удалить комментарий?',
+				onConfirm: () => {
+					dispatch(removeCommentAsync(requestServer, postId, id));
+					dispatch(CLOSE_MODAL);
+				},
+				onCancel: () => dispatch(CLOSE_MODAL),
+			}),
+		);
+	};
 	return (
 		<div className={className}>
 			<div className="comment">
@@ -27,7 +45,12 @@ const CommentContainer = ({ className, id, author, publishedAt, content }) => {
 				</div>
 				<div className="comment-text">{content}</div>
 			</div>
-			<Icon id="fa-trash-o" margin="0 0 0 10px" size="21px" onClick={() => {}} />
+			<Icon
+				id="fa-trash-o"
+				margin="0 0 0 10px"
+				size="21px"
+				onClick={() => onCommentRemove(id)}
+			/>
 		</div>
 	);
 };
