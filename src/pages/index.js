f8f6f259120a5f463821b4ca44964b8d4d77add7
Pterodaktilitsa <pterodaktilitsa@gmail.com>
2023-11-18 21:07:23 +0300

    Реализовать страницу Главная

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export * from './authorization/authorization.js';
 export * from './registration/registration.js';
 export * from './users/users.js';
 export * from './post/post';
+export * from './main/main.js';
