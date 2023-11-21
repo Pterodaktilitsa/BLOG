50beb83c59ec5a1db67d06e6b667718ad29327de
Pterodaktilitsa <pterodaktilitsa@gmail.com>
2023-11-21 16:04:58 +0300

    Реализовать страницу с ошибкой

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -1,17 +1,17 @@
 import { H2 } from '../h2/h2.js';
 import styled from 'styled-components';
+
 const Div = styled.div`
 	display: flex;
 	align-items: center;
 	flex-direction: column;
+	font-size: 18px;
 `;
 
-export const Content = ({ children, error }) =>
-	error ? (
+export const Error = ({ error }) =>
+	error && (
 		<Div>
 			<H2>Ошибка</H2>
 			<div>{error}</div>
 		</Div>
-	) : (
-		children
 	);
